4738ab0c3a8451756ea047252a79b52eefb1e5af
rahul01r <rrdubeynibm@gmail.com>
2022-11-18 16:49:18 +0530

    [rahul] Add:UC1-BinarySearch

---
+++ DataStructureAndAlgorithm/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithm
+{
+    public class BinarySearch
+    {
+        public void Search(string filepath)
+        {
+            string data = File.ReadAllText(filepath);
+            string[] words = data.Split(",");
+            Console.WriteLine("Enter words to search");
+            string search = Console.ReadLine();
+            foreach (var item in words)
+            {
+                if (search.ToLower().Equals(item.ToLower()))
+                {
+                    Console.WriteLine("words found");
+                    return;
+                }
+            }
+            Console.WriteLine("words not found");
+        }
+    }
+}
---
+++ DataStructureAndAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿ namespace DataStructureAndAlgorithm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Binary search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"C:\Users\Lenovo\Desktop\Git\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
+                        break;
+                }
+            }
+        }
+    }
+}
