284a100b5a0c8d58d3417b8b4a60fcab03609d3e
rahul01r <rrdubeynibm@gmail.com>
2022-11-20 09:43:59 +0530

    [rahul] Add:UC2-BubbleSort

--- DataStructureAndAlgorithm/Program.cs
+++ DataStructureAndAlgorithm/Program.cs
@@ -7,13 +7,17 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Select Programs\n 1.Binary search");
+                Console.WriteLine("Select Programs\n 1.Binary search\n 2.Bubble sort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         BinarySearch binarysearch = new BinarySearch();
                         binarysearch.Search(@"C:\Users\Lenovo\Desktop\Git\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
+                        break;
+                    case 2:
+                        BubbleSort bubblesort = new BubbleSort();
+                        bubblesort.Sort(@"C:\Users\Lenovo\Desktop\Git\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
                         break;
                 }
             }
